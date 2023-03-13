f8b8d405812965a28e1a23a234f90bc6ceabd7c2
Gagi <113668410+gagiHerDesign@users.noreply.github.com>
2023-03-13 22:23:31 +0800

    Update VipHome.vue

--- src/pages/vip/VipHome.vue
+++ src/pages/vip/VipHome.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="bg text-center">
-    <h6 style="font-weight: 600;">歡迎來到SUSI會員中心! 請選擇左列項目</h6>
+    <h6 style="font-weight: 600;">歡迎來到SUSI會員中心!! 請選擇左列項目</h6>
     <img src="https://images.unsplash.com/photo-1487700160041-babef9c3cb55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80" alt="">
   </q-page>
 </template>
